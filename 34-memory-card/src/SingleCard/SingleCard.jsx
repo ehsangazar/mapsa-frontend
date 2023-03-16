0e0dec95974d2205b2d1bb04fc7694b7889e17e2
Ehsan Gazar <me@ehsangazar.com>
2023-03-16 19:49:13 +1100

    version 2 of 34

--- 34-memory-card/src/SingleCard/SingleCard.jsx
+++ 34-memory-card/src/SingleCard/SingleCard.jsx
@@ -10,7 +10,23 @@ const SingleCard = ({ data, handleDelete }) => {
   return (
     <li onClick={toggleCard}>
       <h2>{data.title}</h2>
-      {isFlipped && <p>{data.description}</p>}
+
+      {/* MOUNT / UNMOUNT */}
+      {/* {isFlipped && <p className="card-version1">{data.description}</p>} */}
+
+      {/* CSS EDIT */}
+      {/* INLINE */}
+      {/* CSS IN JS */}
+      <p
+        className="card-version2"
+        style={{
+          opacity: isFlipped ? 1 : 0,
+          height: isFlipped ? "100px" : 0,
+          transitionDuration: "1s",
+        }}
+      >
+        {data.description}
+      </p>
       <button onClick={() => handleDelete(data.id)}>Delete</button>
     </li>
   );
